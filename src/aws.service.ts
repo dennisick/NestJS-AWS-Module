afa369c4a36c9642525a6e342d4ed36f218f3f5d
Dennis Ickes <dennis@dendery.de>
2022-10-16 22:08:22 +0200

    Changed

--- src/aws.service.ts
+++ src/aws.service.ts
@@ -23,7 +23,7 @@ export class AWSService {
     const input: DocumentClient.UpdateItemInput = {
       TableName: tableName,
       Key: key,
-      UpdateExpression: 'SET ',
+      UpdateExpression: '',
       ExpressionAttributeNames: {
 
       },
@@ -32,19 +32,41 @@ export class AWSService {
       }
     };
 
-    Object.keys(data).forEach((key, index) => {
+    const set = Object.keys(data).filter(key => data[key]);
+    const remove = Object.keys(data).filter(key => !data[key]);
+
+    console.log(set);
+    console.log(remove);
+
+    input.UpdateExpression = 'SET ';
+    set.forEach((key, index) => {
         const name = '#' + key.toUpperCase();
         const valueName = ':' + key;
 
-        if ((index) === (Object.keys(data).length - 1)) {
+        if ((index) === (Object.keys(set).length - 1)) {
           input.UpdateExpression = input.UpdateExpression + name + ' = ' + valueName;
-        } else{
+        } else {
           input.UpdateExpression = input.UpdateExpression + name + ' = ' + valueName  + ', ';
         }
         
         input.ExpressionAttributeNames[name] = key;
         input.ExpressionAttributeValues[valueName] = data[key];
     });
+
+    if (remove.length > 0) {
+      input.UpdateExpression = input.UpdateExpression + 'REMOVE ';
+      remove.forEach((key, index) => {
+        const name = '#' + key.toUpperCase();
+
+        if (index === (Object.keys(remove).length - 1)) {
+          input.UpdateExpression = input.UpdateExpression + name;
+        } else {
+          input.UpdateExpression = input.UpdateExpression + name + ', ';
+        }
+
+        input.ExpressionAttributeNames[name] = key;
+      })
+    }
 
     return input;
   }
